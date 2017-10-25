7baf60d99c2d98449d03bb491d544d770eb4e260
Andrey Kolybelnikov <a.kolybelnikov@googlemail.com>
2017-10-25 12:53:08 +0200

    Home container, Routes and react-center

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
-import { Layout, Menu, Icon, Button, Row, Col, Affix } from 'antd';
+import { Link } from "react-router-dom";
+import { Layout, Menu, Icon, Button, Affix } from 'antd';
 import './App.css';
+import Routes from "./Routes";
+import Center from 'react-center';
 const logo = require(`./mstile-150x150.png`);
 const { Header, Footer, Content, Sider } = Layout;
 
@@ -10,22 +13,18 @@ class App extends Component {
       <div className="App">
         <Layout>
             <Header className="header">
-              <div className="logo"><img className="image is-64x64" src={logo} alt="logo"/></div> 
-              <Menu className="is-hidden-mobile"
-                theme="light"
-                mode="horizontal"
-                defaultSelectedKeys={['1']}
-                style={{ lineHeight: '64px', color: '#E3BF3F' }}>
+              <Link to="/"><div className="logo"><img className="image is-64x64" src={logo} alt="logo"/></div></Link> 
+                <Menu className="is-hidden-mobile" theme="light" mode="horizontal" defaultSelectedKeys={['1']} style={{ lineHeight: '64px', color: '#E3BF3F' }}>
                 <Menu.Item key="1">Coffee</Menu.Item>
                 <Menu.Item key ="2">Bread</Menu.Item>
                 <Menu.Item key="3">Cakes</Menu.Item>
                 <Menu.Item key="4">Order</Menu.Item>
                 <Menu.Item key="5">About Us</Menu.Item>
                 <Menu.Item key="6">Contact</Menu.Item>
-              </Menu>    
+              </Menu>  
             </Header>
             <Layout>
-              <Sider className="is-hidden-tablet" breakpoint="xl" collapsedWidth="0" onCollapse={(collapsed, type) => { console.log(collapsed, type); }}>
+              <Sider className="is-hidden-tablet" breakpoint="xl" collapsedWidth="0">
                 <Menu theme="light" mode="inline" defaultSelectedKeys={['1']}>
                   <Menu.Item key="1"><Icon type="coffee" />Coffee</Menu.Item>
                   <Menu.Item key="2"><Icon type="shop" />Bread</Menu.Item>
@@ -36,12 +35,11 @@ class App extends Component {
                 </Menu>
               </Sider>
               <Layout>
-                <Content>
-                  <div className="scrollable-container" ref={(node => { this.container = node; })}>          
-                    <Affix target={ () => this.container} style={{ position: 'absolute', top: 70, right: 5}}>
-                      <Button type="primary" className="is-pulled-right is-size-7-mobile is-size-6"><Icon type="phone" /> 8 (095) 124-53-67</Button>
-                    </Affix>
-                  </div>
+                <Content>     
+                  <Affix style={{ position: 'absolute', top: 70, right: 5}}>
+                    <Button type="primary" className="is-pulled-right is-size-7-mobile is-size-6"><Icon type="phone" /> 8 (095) 124-53-67</Button>
+                  </Affix>
+                  <Center><Routes /></Center>             
                 </Content>
                 <Footer>Footer</Footer>
               </Layout>
