04edf04665013de56f31331ba2badaed8ed244e1
Andrey Kolybelnikov <a.kolybelnikov@googlemail.com>
2017-10-26 23:58:49 +0200

    setting the state of the content component by adding dynamic props

--- src/App.js
+++ src/App.js
@@ -1,46 +1,70 @@
 import React, { Component } from 'react';
 import { NavLink } from "react-router-dom";
-import { Layout, Menu, Icon, Button, Affix, Row, Col } from 'antd';
+import { Layout, Menu, Icon, Button, Affix } from 'antd';
 import './App.css';
 import Routes from "./Routes";
 import styled from 'styled-components';
 const logo = require(`./mstile-150x150.png`);
 const { Header, Content, Sider } = Layout;
 const Container = styled.div`
-  max-width: 1010px;
-  margin: 0 auto;
+    max-width: 1010px;
+    margin: 0 auto;
 `;
 const Level = styled.div`
-  background-color: rgba(220, 44, 44, 0.15)
+    background-color: rgba(220, 44, 44, 0.25);
+`;
+const OuterContent = styled(Content)`
+    z-index: 10;
+`;
+const InnerContainer = styled.div.attrs({
+  height: props => props.height
+})`
+  max-width: 910px;
+  margin: 0 auto;
+  min-height: ${props => props.height}px;
 `;
 
 class App extends Component {
 
   state = {
-      current: '0',
-      size: 'large'
+    current: '0',
+    height: ''
+  } 
+
+  constructor(props) {
+      super(props);
+      this.state = { height: window.innerHeight - 103 };
+      this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+  }
+
+  componentDidMount() {
+      this.updateWindowDimensions();
+      window.addEventListener('resize', this.updateWindowDimensions);
+  }
+  
+  componentWillUnmount() {
+      window.removeEventListener('resize', this.updateWindowDimensions);
   }
 
   handleLogoClick = (e) => {
-    this.setState({
-      current: "0"
-    });
+      this.setState({
+        current: "0"
+      });
   }
 
   handleClick = (e) => {
-    this.setState({
-      current: e.key,
-    });
+      this.setState({
+        current: e.key,
+      });
   }
 
-  handleSizeChange = (e) => {
-    this.setState({ size: e.target.value });
+  updateWindowDimensions() {
+      this.setState({ height: window.innerHeight - 103 });
+      console.log(this.state.height)
   }
 
   render() {
-    const size = this.state.size;
     return (
-
         <Layout>
           <Sider style={{ overflow: 'visible', position: 'fixed', left: 10, zIndex: 20, top: 20 }} className="is-hidden-tablet" breakpoint="xl" collapsedWidth="0">
             <Menu onClick={this.handleClick} selectedKeys={[this.state.current]} theme="light" mode="inline">
@@ -55,7 +79,7 @@ class App extends Component {
             </Menu>
           </Sider>
             
-          <Layout>
+          <Layout style={{ background: "white" }} >
             <Header className="header">
               <Container>
                 <NavLink onClick={this.handleLogoClick} to="/"><div className="logo"><img className="image is-64x64" src={logo} alt="logo"/></div></NavLink> 
@@ -71,24 +95,24 @@ class App extends Component {
                 </Menu>  
               </Container>
             </Header>
-            <Content style={{ overflow: 'initial', zIndex: 10 }}>     
-              <Affix style={{ position: 'absolute', top: 70, right: '5%', zIndex: 20}}>
+            <OuterContent>     
+              <Affix style={{ position: 'absolute', top: 70, right: '10%', zIndex: 20}}>
                 <Button type="primary" className="is-pulled-right is-size-7-mobile is-size-6"><Icon type="phone" /> 8 (095) 124-53-67</Button>
               </Affix>
-              <Container><Routes /></Container>          
-            </Content>
-            <Affix offsetBottom={0}>
+              <InnerContainer height={this.state.height}><Routes /></InnerContainer>         
+            </OuterContent>
+            <Affix offsetBottom={0} style={{ zIndex: 20 }}>
               <Container>
                 <Level>
                   <nav className="level is-mobile">            
                     <div className="level-item">
-                        <a className="has-text-danger"><i class="fa fa-instagram fa-2x" aria-hidden="true"></i></a>
+                        <a className="has-text-danger"><i className="fa fa-instagram fa-2x" aria-hidden="true"></i></a>
                     </div>
                     <div className="level-item">
-                        <a className="has-text-info"><i class="fa fa-facebook fa-2x" aria-hidden="true"></i></a>
+                        <a className="has-text-info"><i className="fa fa-facebook fa-2x" aria-hidden="true"></i></a>
                     </div>
                     <div className="level-item">
-                        <a className="has-text-black-ter"><i class="fa fa-vk fa-2x" aria-hidden="true"></i></a>
+                        <a className="has-text-black-ter"><i className="fa fa-vk fa-2x" aria-hidden="true"></i></a>
                     </div>
                   </nav>
                 </Level>
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -6,10 +6,14 @@ import styled from 'styled-components';
 import ProgressiveImage from 'react-progressive-bg-image';
 
 const bgImg = require(`./bg.jpg`);
-const StyledProgressiveImage = styled(ProgressiveImage)`
+const Background = styled(ProgressiveImage)`
   height: 100%;
+  background-color: pink;
   background-size: contain;
   background-position: center center;
+`;
+const Instafeed = styled.div`
+  background-color: white;
 `;
 
 export default class Home extends Component {
@@ -19,24 +23,19 @@ export default class Home extends Component {
     this.state = {
       src: '',
       err: null,
-      image: 'https://source.unsplash.com/eEKgcdGZhzs/500x750'
+      image: 'https://source.unsplash.com/eEKgcdGZhzs/850x1350'
     }
   }  
 
   render() {
     return (
       <Row>
-        <Col className="" sm={17}>
-          <div className="Home">    
-            <Center>
-              <div className="lander">
-                <StyledProgressiveImage src={this.state.image} placeholder={bgImg} transition="all 1s linear"><Center>Let's try to put sme text in here</Center></StyledProgressiveImage>          
-              </div>
-            </Center>           
-          </div>
-        </Col>
-        <Col sm={6}>
-         
+        <Col className="is-hidden-mobile" sm={6}></Col>
+        <Col className="is-hidden-mobile" sm={6}></Col>
+        <Col className="" xs={24} sm={12}>   
+          <Center>   
+              <Background src={this.state.image} placeholder={bgImg} transition="all 1s linear"><Center>Let's try to put sme text in here</Center></Background>          
+          </Center>           
         </Col>
       </Row>
     );
