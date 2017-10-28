053ed9b16515b5e5e823307f5ad4be733127750a
Andrey Kolybelnikov <a.kolybelnikov@googlemail.com>
2017-10-29 00:22:14 +0200

    separating authentication logic and accessing the user session

---
+++ src/libs/awsLib.js
@@ -0,0 +1,36 @@
+import { CognitoUserPool } from "amazon-cognito-identity-js";
+import config from "../config";
+
+export async function authUser() {
+    const currentUser = getCurrentUser();
+
+    if (currentUser === null) {
+        return false;
+    }
+
+    await getUserToken(currentUser);
+
+    return true;
+}
+
+function getUserToken(currentUser) {
+    return new Promise((resolve, reject) => {
+        currentUser.getSession(function(err, sessiom) {
+            if (err) {
+                reject(err);
+                return;
+            }
+
+            resolve(session.getIdToken().getJwtToken());
+        });
+    });
+}
+
+function getCurrentUser() { 
+    const userPool = new CognitoUserPool({
+        UserPoolId: config.cognito.USER_POOL_ID,
+        ClientId: config.cognito.APP_CLIENT_ID
+    });
+
+    return userPool.getCurrentUser();
+}
