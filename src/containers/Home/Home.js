62736183ed5b1e70bdb5c1eaf2b0ee41aeab7d88
Andrey Kolybelnikov <a.kolybelnikov@googlemail.com>
2017-11-11 00:59:07 +0100

    fixing instafeed rendering issue and styling home component

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,22 +1,22 @@
 import React, { Component } from "react";
-import { Row, Col, Card } from 'antd';
+import { Row, Col, Card, Icon } from 'antd';
 import Center from 'react-center';
 import "./Home.css";
 import styled, { keyframes } from 'styled-components';
-import { bounceInUp } from 'react-animations';
+import { bounceInUp, slideInLeft } from 'react-animations';
 import ProgressiveImage from 'react-progressive-bg-image';
 import Instafeed from 'react-instafeed';
 import config from "../../config";
 
 const bounceAnimation = keyframes`${bounceInUp}`;
+const slideInAnimation = keyframes`${slideInLeft}`;
 
 const bgImg = require(`./bg.jpg`);
 
 const Background = styled(ProgressiveImage)`
   background-size: cover;
   background-position: center center;
-  min-height: 670px;
-  margin-left:5px;
+  min-height: 690px;
 `;
 
 const Template = ` 
@@ -34,67 +34,57 @@ const InstaLoad = styled(Row)`
   animation: 2s ${bounceAnimation};
 `
 
+const GoButton = styled(Card)`
+  cursor: pointer;
+  animation: 1s ${slideInAnimation};
+`
+function goToProducts() {
+  console.log('clicked');
+}
+
 export default class Home extends Component {
 
   constructor(props) {
     super(props);
 
     this.state = {
-      image: 'https://source.unsplash.com/eEKgcdGZhzs/500x750'
+      image: 'https://source.unsplash.com/eEKgcdGZhzs/500x750',
+      loaded: false
     }
   }
 
-  beforeLoad() {
-    console.log('going to fetch');
-  }
-
-  afterLoad() {
-    console.log('loaded');
-  }
-
-  onSuccess() {
-    console.log('success!');
-    //this.setState({loaded: true});
-  }
-
-  componentDidMount() {
-    
-  }
-
-  componentWillUnmount() {
-    console.log('unmounting');
+  shouldComponentUpdate() {
+    return false;
   }
 
   render() {   
-    return (
-
-        
-          <Row>
-            <Col sm={14}>
-                <InstaCard title="Our news on Instagram" bordered="false">
-                  <InstaLoad type="flex" justify="start" id='instafeed'>              
-                    <Instafeed ref={instafeed => this.instafeed = instafeed}
-                      limit='16'
-                      resolution='thumbnail'
-                      sortBy='most-recent'
-                      template={Template}
-                      userId={`${config.instagram.REACT_APP_INSTAGRAM_USER_ID}`}
-                      clientId={`${config.instagram.REACT_APP_INSTAGRAM_CLIENT_ID}`}
-                      accessToken={`${config.instagram.REACT_APP_INSTAGRAM_ACCESS_TOKEN}`}
-                      before={this.beforeLoad()}
-                      after={this.afterLoad()}
-                      success={this.onSuccess()}
-                    />  
-                  </InstaLoad>
-                </InstaCard>          
-            </Col> 
-            <Col sm={10}>
-              <Background src={this.state.image} placeholder={bgImg} transition="all 1s linear">
-              </Background>
-            </Col> 
-          </Row>         
-        
-
+    return (        
+      <Row>
+        <Col sm={14}>
+            <InstaCard title="Our news and offers on Instagram" bordered="false">
+              <InstaLoad type="flex" justify="start" id='instafeed'>              
+                <Instafeed ref={instafeed => this.instafeed = instafeed}
+                  limit='16'
+                  resolution='thumbnail'
+                  sortBy='most-recent'
+                  template={Template}
+                  userId={`${config.instagram.REACT_APP_INSTAGRAM_USER_ID}`}
+                  clientId={`${config.instagram.REACT_APP_INSTAGRAM_CLIENT_ID}`}
+                  accessToken={`${config.instagram.REACT_APP_INSTAGRAM_ACCESS_TOKEN}`}
+                />  
+              </InstaLoad>
+            </InstaCard>
+            <GoButton onClick={ goToProducts }>
+              <Center>
+                  <p><span className="ant-card-head-title">Go to the products<Icon type="right" /></span></p>
+              </Center>
+            </GoButton>          
+        </Col> 
+        <Col sm={10}>
+          <Background src={this.state.image} placeholder={bgImg} transition="all 1s linear">
+          </Background>
+        </Col> 
+      </Row>       
     );
   }
 }
--- src/containers/NewProduct/NewProduct.js
+++ src/containers/NewProduct/NewProduct.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 // import { CognitoUserPool, AuthenticationDetails, CognitoUser } from "amazon-cognito-identity-js";
-import { Form, Icon, Input, Upload, message, Button, Select } from 'antd';
+import { Form, Icon, Input, Upload, Button, Select } from 'antd';
 import Center from 'react-center';
 import config from "../../config";
 import LoaderButton from "../../components/LoaderButton";
