a865cd501815e981db0d07113d4944cb24269b2a
Andrey Kolybelnikov <a.kolybelnikov@googlemail.com>
2017-12-19 16:15:27 +0100

    styling the product page

--- src/containers/Product/Product.js
+++ src/containers/Product/Product.js
@@ -1,28 +1,43 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { Row, Col, Card, Breadcrumb, Icon } from 'antd';
+import ProgressiveImage from "react-progressive-bg-image";
 import styled, { keyframes } from 'styled-components';
 import { zoomIn } from 'react-animations';
 import config from "../../config";
 import { invokeOpenApi } from "../../libs/awsLib";
 import "./Product.css";
+import Center from "react-center";
 
 const zoomInAnimation = keyframes`${zoomIn}`;
 
+const bgImg = require(`../../public/bg.jpg`);
+
 const ProductRow = styled(Row)`
     @media only screen and (max-width: 480px) {
-        margin: 10% 0 25% 0;
+        margin: 25% 0;
     } 
     @media only screen and (min-width: 680px) and (max-width: 768px) {
         margin: 17.5% 0;
     }
 `;
 
+const ProductImage = styled(ProgressiveImage)`
+    background-size: contain;
+    background-position: center center;
+    height: 500px;
+    width: 500px;
+    @media only screen and (max-width: 480px) {
+        width: 300px;
+        height: 300px;
+    }
+`
+
 const ProductCard = styled(Card)`
     animation: 1s ${zoomInAnimation};
 `;
 const Breadcrumbs = styled(Row)`
-    margin-top: 35px;
+    margin: 10px 0;
 `;
 
 export default class Product extends Component {
@@ -50,21 +65,11 @@ export default class Product extends Component {
         return invokeOpenApi({ path: `/products/${this.props.match.params.id}/${this.props.location.pathname.split('/')[2]}` });
     }
 
-    getContent() {
-        return(
-            <div>
-                <img alt="" src={`${config.s3.URL}/750x750/${this.state.product.image}`} />
-            </div>
-        )
-    }
-
     renderProduct(product) {
         return(
             <Col xs={{ span: 20, offset: 2 }} sm={{ span: 18, offset: 3 }} md={{ span: 16, offset: 4 }} >
                 <ProductCard title={product && product.productName}>
-                    <div>
-                        <p style={{textAlign: 'center', color: '#52082D'}} className="is-size-6-desktop is-size-7-mobile">{product.content}</p>
-                    </div>
+                    <Center><ProductImage src={`${config.s3.URL}/500x500/${this.state.product.image}`}  placeholder={bgImg} transition="all 1s linear" /></Center>
                     <div>
                         <Row>
                             <Col xs={12}><p className="is-size-5-desktop is-size-7-mobile is-size-6-tablet" style={{textAlign: 'left', color: '#331507'}}><span style={{color: '#EACCB2'}}>Вес: </span>{product && product.weight}</p></Col>
@@ -79,7 +84,7 @@ export default class Product extends Component {
     render() {
         return (
             <div>
-                <Row className="is-hidden-tablet" style={{marginTop: "10px"}}><Icon onClick={this.props.history.goBack} className="icon-back" type="left" /></Row>
+                <Row className="is-hidden-tablet" style={{marginTop: "35px"}}><Icon onClick={this.props.history.goBack} className="icon-back" type="left" /></Row>
                 <Breadcrumbs className="is-hidden-mobile">
                     <Breadcrumb separator=">">
                         <Breadcrumb.Item><Link to="/">Новинки</Link></Breadcrumb.Item>
