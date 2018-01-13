4bf0f65cdb3e7df4484ebecdcf4e973f16ccfa32
Andrey Kolybelnikov <a.kolybelnikov@googlemail.com>
2018-01-13 21:14:11 +0100

    home component refactored

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
-import { Card, Modal, Row, Col, Carousel, Spin } from "antd";
+import { Card, Modal, Row, Col } from "antd";
 import config from "../../config";
 import { invokeOpenApi } from "../../libs/awsLib";
 import styled, { keyframes } from "styled-components";
 import { zoomIn } from "react-animations";
 import ProgressiveImage from "react-progressive-bg-image";
 import Instafeed from "../../components/Instafeed";
+import NewsFeed from "../../components/NewsFeed";
 import Responsive from 'react-responsive';
 import LazyLoad from 'react-lazyload';
 import "./Home.css";
@@ -17,10 +18,9 @@ const bread = require(`../../public/categories/bread.jpg`);
 const coffee = require(`../../public/categories/coffee.jpg`);
 const cakes = require(`../../public/categories/cakes.jpg`);
 const order = require(`../../public/categories/order.jpg`);
-const offerImg = require(`../../public/logo.png`);
+const logoImg = require(`../../public/logo.png`);
 
-const Desktop = props => <Responsive {...props} minWidth={992} />;
-const Tablet = props => <Responsive {...props} minWidth={481} maxWidth={991} />;
+const Tablet = props => <Responsive {...props} minWidth={481} />;
 const Mobile = props => <Responsive {...props} maxWidth={480} />;
 
 const ImageCard = styled(ProgressiveImage)`
@@ -32,10 +32,6 @@ const OfferCard = styled(ProgressiveImage)`
   background-size: cover;
   background-position: center;
   min-height: 200px;
-`
-
-const Instacard = styled(Card)`
-  animation: 2s ${zoomInAnimation};
 `
 
 const ModalImage = styled(ProgressiveImage)`
@@ -50,20 +46,6 @@ const ModalImage = styled(ProgressiveImage)`
   }
 `
 
-const NewsImage = styled(ProgressiveImage)`
-  background-size: cover;
-  background-position: center;
-  min-width: 180px;
-  min-height: 180px;
-  @media only screen and (min-width: 481px) and (max-width: 768px) {
-    min-width: 150px;
-    min-height: 150px;
-  }
-  @media only screen and (max-width: 480px) {
-    min-width: 75px;
-    min-height: 75px;
-  }
-`
 const CategoryCard = styled(Col)`
   animation: 1.5s ${zoomInAnimation};
   cursor: pointer;
@@ -110,19 +92,6 @@ export default class Home extends Component {
         offercontent: offer.content
        });
 
-      const news = [];
-      const fetchedNews = await this.getNews();
-      fetchedNews.reverse();
-      for (let i = 0; i < 5; i++) {
-        if (fetchedNews[i] !== undefined) {
-          news.push(fetchedNews[i]);
-        }
-      }
-      this.setState({ 
-        news: news,
-        isLoading: false
-      });
-
     } catch (e) {
       console.log(e);
     }
@@ -156,24 +125,7 @@ export default class Home extends Component {
 
   renderOffer() {
     if (this.state.offercontent && this.state.offerimage) 
-      return <OfferCard src={`${config.s3.URL}/250x250/${this.state.offerimage}`} placeholder={offerImg} transition="all 1s linear" crossOrigin='anonymous' />;
-  }
-
-  getNews() {
-      return invokeOpenApi({ path: "/news"});
-  }
-
-  renderNews(news) {
-    if (this.state.news) 
-      return news.map(
-        (newsitem) => 
-          <a key={newsitem.newsId} href='/news'>
-            <Row style={{marginRight: '20px'}}>  
-              <Col xs={6}><Card bordered={false} cover={<NewsImage src={`${config.s3.URL}/200x200/${newsitem.image}`} placeholder={offerImg} transition="all 1s linear" crossOrigin='anonymous'/>}></Card></Col>
-              <Col xs={{ span: 16, offset: 2 }} sm={{ span: 17, offset: 1 }}><Card className='news-card' bordered={false}><p className='is-size-7-mobile is-size-5-tablet news-text' style={{textAlign: "center"}}><Mobile>{newsitem.content.substring(0, 100)} ... </Mobile><Tablet>{newsitem.content.substring(0, 250)} ... </Tablet><Desktop>{newsitem.content.substring(0, 300)} ... </Desktop></p></Card></Col>
-            </Row>
-          </a>
-      )
+      return <OfferCard src={`${config.s3.URL}/250x250/${this.state.offerimage}`} placeholder={logoImg} transition="all 1s linear" crossOrigin='anonymous' />;
   }
 
   setModalVisible(modalVisible) {
@@ -206,34 +158,22 @@ export default class Home extends Component {
               visible={this.state.modalVisible}  
               onOk={() => this.setModalVisible(false)} 
               onCancel={() => this.setModalVisible(false)}> 
-              <ModalImage crossOrigin='anonymous' src={`${config.s3.URL}/500x500/${this.state.offerimage}`} placeholder={offerImg} transition="all 1s linear"  />  
+              <ModalImage crossOrigin='anonymous' src={`${config.s3.URL}/500x500/${this.state.offerimage}`} placeholder={logoImg} transition="all 1s linear"  />  
           </Modal>
         </div>
-        <div className="tile is-ancestor">
-          <div className="tile is-parent is-3 is-hidden-mobile">
-            <article className="tile is-child box">
-              <LazyLoad once height={200}>
-                <ImageCard src={`${config.s3.URL}/300x450/photo-1498049281100-cb3c002220f5.jpg`} placeholder={bgImg} transition="all 1s linear" /> 
-              </LazyLoad> 
-            </article>
-          </div>
-          <div className="tile is-vertical is-parent">
-          <LazyLoad once height={200}>
-            <article className="tile is-child box">              
-              <Instacard title="Мы на Instagram" bordered="true">                         
-                <Instafeed />
-              </Instacard>   
-            </article>
-            </LazyLoad> 
-          </div>
-        </div>
-        <div style={{marginTop: "25px", marginBottom: "35px", background: "rgba(234,204,178,.5)", padding: ".7rem"}}>
-            <Card style={{cursor: "pointer"}} title="Наши новости" bordered="false">            
-              <Carousel autoplaySpeed={5000} autoplay>
-                {this.state.news ? this.renderNews(this.state.news) : <Spin style={{display: 'block'}} size="small" />}
-              </Carousel>           
-            </Card>
-        </div>
+        <Tablet>
+          <div className="tile is-ancestor">
+            <div className="tile is-parent is-3 is-hidden-mobile">
+              <article className="tile is-child box">
+                <LazyLoad once height={200}>
+                  <ImageCard src={`${config.s3.URL}/300x450/photo-1498049281100-cb3c002220f5.jpg`} placeholder={bgImg} transition="all 1s linear" /> 
+                </LazyLoad> 
+              </article>
+            </div>      
+            <Instafeed /> 
+          </div>       
+          <NewsFeed />
+        </Tablet>
       </div>
     );
   }
